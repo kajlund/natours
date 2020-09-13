608d392eb89849ba14d1150e2bbbe2dcde9f11ad
Kaj Lund <kaj.lund@gmail.com>
2020-09-13 17:22:05 +0300

    Adds tours post endpoint for creating a new Tour

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+const fs = require('fs');
+
 const express = require('express');
+
+const tours = require('./dev-data/data/tours-simple.json');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Middleware
+app.use(express.json());
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
