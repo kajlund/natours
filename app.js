79e9c1404fd9987cc9a98a7f78c128da1de4bdbc
Kaj Lund <kaj.lund@gmail.com>
2020-09-14 18:00:54 +0300

    Adds static files handler for public folder

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   consola.info('Middleware says hi! 👋');
