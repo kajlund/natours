9511e478641cbfe57f86d38b7024019c11d47237
Kaj Lund <kaj.lund@gmail.com>
2020-09-14 18:21:30 +0300

    Adds handling of env variables

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 const app = express();
 
 // Middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const consola = require('consola');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = require('./app');
 
+consola.info(`Running in "${app.get('env')}" environment`);
+
 // Start Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   consola.info(`App running on port ${PORT}`);
 });
